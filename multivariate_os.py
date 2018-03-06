89de242f8f509e1ae7539535dcafe3457a3bf826
baibai25 <kamuy.camel.0225@gmail.com>
2018-03-06 23:01:10 +0900

    add to-do

--- multivariate_os.py
+++ multivariate_os.py
@@ -1,27 +1,50 @@
 import numpy as np
 import pandas as pd
 
-def find_zerostd(pos):
-    
+def find_zerostd(pos): 
     print("Searching zero std...")
     std = pos.std()
     mean = pos.mean()
-
     zero_list = []
     zero_mean = []
 
     for i in range(len(pos.columns)):
         if std[i] == 0:
-            print("Found zero std!")
-            #print(pos.columns[i])
+            print(pos.columns[i] + " : Found zero std!")
             zero_list.append(pos.columns[i])
             zero_mean.append(mean[i])
             pos = pos.drop(pos.columns[i], axis=1)
+    
+    if (len(zero_list) == 0) and (len(zero_mean) == 0): 
+        print("Not found zero std.")
     #print(zero_list)
     #print(zero_mean)
     print("Finished.")
     return zero_list, zero_mean, pos
-    
+
+#######################################
+# TO-DO
+# 1. extract no_corr attribute values
+# 2. calc mean and var
+# 3. create normal dist
+#######################################
+
+def no_corr(pos):
+    corr = abs(pos.corr())
+    nocorr_list = []    
+
+    for i in range(len(corr.columns)):
+        sort = corr.iloc[:, [i]]
+        sort = sort.sort_values(by=sort.columns[0], ascending=False) #sort
+        
+        if sort.values[1] < 0.2:
+            print("Found no correlation")
+            print(sort)
+            print(sort.values[1])
+            nocorr_list.append(pos.columns[i])
+            pos = pos.drop(pos.columns[i], axis=1)
+    #return pos
+
 def mnd_os(zero_list, zero_mean, pos, num_minority):
     #calc correlation and covert absolute value
     corr = abs(pos.corr())
@@ -33,6 +56,7 @@ def mnd_os(zero_list, zero_mean, pos, num_minority):
     for i in range(len(corr.columns)):
         sort = corr.iloc[:, [i]] #extract one index
         sort = sort.sort_values(by=sort.columns[0], ascending=False) #sort
+        #print(sort)
         corr_col.append(sort.columns[0]) #strong corr coulumns
         corr_ind.append(sort.index[1]) #strong corr index
     #print(corr_col)
@@ -60,8 +84,7 @@ def mnd_os(zero_list, zero_mean, pos, num_minority):
     if len(zero_list) != 0:
         for i in range(len(zero_list)):
             df[zero_list[i]] = zero_mean[i]
-            
+            print(zero_list[i])
     df['Label'] = 1
     #df.to_csv('/home/yura/Desktop/mlpd_train.csv', index=False)
     return df
-    #df
