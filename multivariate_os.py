f9eb49fe05c7f69bf269de07a09c9fe493550e8e
baibai25 <kamuy.camel.0225@gmail.com>
2018-03-08 22:06:45 +0900

    add tqdm status line

--- multivariate_os.py
+++ multivariate_os.py
@@ -1,33 +1,35 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 # Find zero standard deviation
 def find_zerostd(pos, num_minority): 
-    print("Searching zero std...")
-    std = pos.std()
-    mean = pos.mean()
-    zero_list = []
-    zero_mean = []
+    
+    for i in tqdm(range(100), desc="Searching zero std"):
+        #print("Searching zero std...")
+        std = pos.std()
+        mean = pos.mean()
+        zero_list = []
+        zero_mean = []
 
-    for i in range(len(pos.columns)):
-        if std[i] == 0:
-            print(pos.columns[i] + " : Found zero std!")
-            zero_list.append(pos.columns[i])
-            zero_mean.append(mean[i])
-    #print(zero_list)
-    #print(zero_mean)
+        for i in range(len(pos.columns)):
+            if std[i] == 0:
+                zero_list.append(pos.columns[i])
+                zero_mean.append(mean[i])
+        #print(zero_list)
+        #print(zero_mean)
 
     if (len(zero_list) == 0) and (len(zero_mean) == 0): 
         print("Not found zero std.")
         df = None
     else:
+        print("Found! {}".format(zero_list))
         df_index = np.zeros(shape=(num_minority, len(zero_list)))
         df = pd.DataFrame(df_index, columns=zero_list)
 
         for i in range(len(zero_list)): 
             pos.drop(zero_list[i], axis=1, inplace=True)
             df[zero_list[i]] = zero_mean[i]
-        print("Finished.")
     #print(pos)
     #print(df)
     return pos, df
@@ -35,29 +37,30 @@ def find_zerostd(pos, num_minority):
 
 # Find no correlation and univariate sampling
 def no_corr(pos, num_minority):
-    print("Searching no correlation...")
-    corr = abs(pos.corr())
-    nocorr_df = pd.DataFrame(index=[], columns=[]) 
-    mean_list = []
-    var_list = []
-    col_list = []
 
-    # split no corr attribute and calc mean and var.
-    for i in range(len(corr.columns)):
-        sort = corr.iloc[:, [i]]
-        sort = sort.sort_values(by=sort.columns[0], ascending=False) #sort
-        
-        if sort.values[1] < 0.2:
-            print(pos.columns[i] + " : Found no correlationi!")
-            #print(sort.values[1])
-            mean_list.append(pos[pos.columns[i]].mean())
-            var_list.append(pos[pos.columns[i]].var())
-            col_list.append(pos.columns[i])
+    for i in tqdm(range(100), desc="Searching no correlation"):
+        corr = abs(pos.corr())
+        nocorr_df = pd.DataFrame(index=[], columns=[]) 
+        mean_list = []
+        var_list = []
+        col_list = []
+
+        # split no corr attribute and calc mean and var.
+        for i in range(len(corr.columns)):
+            sort = corr.iloc[:, [i]]
+            sort = sort.sort_values(by=sort.columns[0], ascending=False) #sort
+            
+            if sort.values[1] < 0.2:
+                #print(sort.values[1])
+                mean_list.append(pos[pos.columns[i]].mean())
+                var_list.append(pos[pos.columns[i]].var())
+                col_list.append(pos.columns[i])
     
     if (len(mean_list)==0) and (len(var_list)==0) and (len(col_list)==0): 
         print("Not found no correlation.")
         df = None
-    else: 
+    else:
+        print("Found! {}".format(col_list))
         # univariate normal dist over-sampling
         tmp = []
         np.random.seed(seed=6)
@@ -79,40 +82,42 @@ def no_corr(pos, num_minority):
 
 # Multivariate sampling
 def mnd_os(pos, num_minority):
-    # calc correlation and covert absolute value
-    corr = abs(pos.corr())
-    #print(corr)
-    
-    # find strong correlation attribute
-    corr_col = []
-    corr_ind = []
-    for i in range(len(corr.columns)):
-        sort = corr.iloc[:, [i]] #extract one index
-        sort = sort.sort_values(by=sort.columns[0], ascending=False) #sort
-        #print(sort)
-        corr_col.append(sort.columns[0]) #strong corr coulumns
-        corr_ind.append(sort.index[1]) #strong corr index
-    #print(corr_col)
-    #print(corr_ind)
-    
-    # calc mean and covariance
-    mean_list = []
-    cov_list = []
-    for i in range(len(pos.columns)):
-        mean_list.append([pos[corr_col[i]].mean(), pos[corr_ind[i]].mean()])
-        cov_list.append(pd.concat([pos[corr_col[i]], pos[corr_ind[i]]], axis=1).cov())
+
+    for i in tqdm(range(100), desc="Multi normal dist over-sampling"):
+        # calc correlation and covert absolute value
+        corr = abs(pos.corr())
+        #print(corr)
         
-    # generate new sample
-    tmp = []
-    np.random.seed(seed=6)
-    for mean, cov in zip(mean_list, cov_list):
-        mul_x, mul_y = np.random.multivariate_normal(mean, cov, num_minority).T
-        tmp.append(mul_x)
-    
-    # convert to dataframe
-    df = pd.DataFrame(tmp).T
-    df.columns = pos.columns
-    #print(df)
+        # find strong correlation attribute
+        corr_col = []
+        corr_ind = []
+        for i in range(len(corr.columns)):
+            sort = corr.iloc[:, [i]] #extract one index
+            sort = sort.sort_values(by=sort.columns[0], ascending=False) #sort
+            #print(sort)
+            corr_col.append(sort.columns[0]) #strong corr coulumns
+            corr_ind.append(sort.index[1]) #strong corr index
+        #print(corr_col)
+        #print(corr_ind)
+        
+        # calc mean and covariance
+        mean_list = []
+        cov_list = []
+        for i in range(len(pos.columns)):
+            mean_list.append([pos[corr_col[i]].mean(), pos[corr_ind[i]].mean()])
+            cov_list.append(pd.concat([pos[corr_col[i]], pos[corr_ind[i]]], axis=1).cov())
+            
+        # generate new sample
+        tmp = []
+        np.random.seed(seed=6)
+        for mean, cov in zip(mean_list, cov_list):
+            mul_x, mul_y = np.random.multivariate_normal(mean, cov, num_minority).T
+            tmp.append(mul_x)
+        
+        # convert to dataframe
+        df = pd.DataFrame(tmp).T
+        df.columns = pos.columns
+        #print(df)
     return df
 
 def append_data(pos, zero_std, no_corr):
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import sys
 import os.path
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import multivariate_os
@@ -45,31 +46,32 @@ pos, no_corr = multivariate_os.no_corr(pos, num_minority)
 pos = multivariate_os.mnd_os(pos, num_minority)
 df = multivariate_os.append_data(pos, zero_std, no_corr)
 
-## Learning
-# df + train data
+
+## Preprocessing
+ # df + train data
 X_mlpd = df.drop('Label', axis=1)
 y_mlpd = df.Label
 X_mlpd = pd.concat([X_mlpd, X_train])
 y_mlpd = pd.concat([y_mlpd, y_train])
 print('y_mlpd: {}'.format(Counter(y_mlpd)))
 
-#Apply over-sampling
-print("Applying over-sampling...")
-sm_reg = over_sampling.SMOTE(kind='regular', random_state=RANDOM_STATE)
-ada = over_sampling.ADASYN(random_state=RANDOM_STATE)
-rand = over_sampling.RandomOverSampler(random_state=RANDOM_STATE)
-X_reg, y_reg = sm_reg.fit_sample(X_train, y_train)
-X_ada, y_ada = ada.fit_sample(X_train, y_train)
-X_rand, y_rand = rand.fit_sample(X_train, y_train)
+for i in tqdm(range(100), desc="Preprocessing"):
+    #Apply over-sampling
+    sm_reg = over_sampling.SMOTE(kind='regular', random_state=RANDOM_STATE)
+    ada = over_sampling.ADASYN(random_state=RANDOM_STATE)
+    rand = over_sampling.RandomOverSampler(random_state=RANDOM_STATE)
+    X_reg, y_reg = sm_reg.fit_sample(X_train, y_train)
+    X_ada, y_ada = ada.fit_sample(X_train, y_train)
+    X_rand, y_rand = rand.fit_sample(X_train, y_train)
 
-# normalize
-X_train = normalize(X_train, norm='l2')
-X_reg = normalize(X_reg, norm='l2')
-X_ada = normalize(X_ada, norm='l2')
-X_rand = normalize(X_rand, norm='l2')
-X_mlpd = normalize(X_mlpd, norm='l2')
-os_list = [[X_train, y_train], [X_reg, y_reg],
-           [X_ada, y_ada], [X_rand, y_rand], [X_mlpd, y_mlpd]]
+    # normalize
+    X_train = normalize(X_train, norm='l2')
+    X_reg = normalize(X_reg, norm='l2')
+    X_ada = normalize(X_ada, norm='l2')
+    X_rand = normalize(X_rand, norm='l2')
+    X_mlpd = normalize(X_mlpd, norm='l2')
+    os_list = [[X_train, y_train], [X_reg, y_reg],
+               [X_ada, y_ada], [X_rand, y_rand], [X_mlpd, y_mlpd]]
 
 # convert classification report to dataframe
 def report_to_df(report):
@@ -78,47 +80,46 @@ def report_to_df(report):
     return(report_df)
 
 # start learning
-print("Start Learning")
-svm_clf = []
-svm_df = pd.DataFrame(index=[], columns=[])
-auc_list = []
+for i in tqdm(range(100), desc="Learning"):
+    svm_clf = []
+    svm_df = pd.DataFrame(index=[], columns=[])
+    auc_list = []
 
-# svm
-for i in range(len(os_list)):
-    svm_clf.append(svm.SVC(random_state=RANDOM_STATE, probability=True, class_weight='balanced').fit(os_list[i][0], os_list[i][1]))
-    
-for i in range(len(svm_clf)):
-    pred = classification_report_imbalanced(y_test, svm_clf[i].predict(X_test))
-    svm_df = svm_df.append(report_to_df(pred))
-    #calc auc
-    prob = svm_clf[i].predict_proba(X_test)[:,1]
-    fpr, tpr, thresholds = roc_curve(y_test, prob)
-    roc_auc_area = auc(fpr, tpr)
-    #print('AUC={}'.format(roc_auc_area))
-    auc_list.append(roc_auc_area)
+    # svm
+    for i in range(len(os_list)):
+        svm_clf.append(svm.SVC(random_state=RANDOM_STATE, probability=True, class_weight='balanced').fit(os_list[i][0], os_list[i][1]))
+        
+    for i in range(len(svm_clf)):
+        pred = classification_report_imbalanced(y_test, svm_clf[i].predict(X_test))
+        svm_df = svm_df.append(report_to_df(pred))
+        #calc auc
+        prob = svm_clf[i].predict_proba(X_test)[:,1]
+        fpr, tpr, thresholds = roc_curve(y_test, prob)
+        roc_auc_area = auc(fpr, tpr)
+        #print('AUC={}'.format(roc_auc_area))
+        auc_list.append(roc_auc_area)
 
-#k-NN
-k=3
-knn_clf = []
-knn_df = pd.DataFrame(index=[], columns=[])
+    #k-NN
+    k=3
+    knn_clf = []
+    knn_df = pd.DataFrame(index=[], columns=[])
 
-for i in range(len(os_list)):
-    knn_clf.append(KNeighborsClassifier(n_neighbors=k).fit(os_list[i][0], os_list[i][1]))
-    
-for i in range(len(knn_clf)):
-    pred = classification_report_imbalanced(y_test, knn_clf[i].predict(X_test))
-    knn_df = knn_df.append(report_to_df(pred))
-    #calc auc
-    prob = knn_clf[i].predict_proba(X_test)[:,1]
-    fpr, tpr, thresholds = roc_curve(y_test, prob)
-    roc_auc_area = auc(fpr, tpr)
-    #print('AUC={}'.format(roc_auc_area))
-    auc_list.append(roc_auc_area)
+    for i in range(len(os_list)):
+        knn_clf.append(KNeighborsClassifier(n_neighbors=k).fit(os_list[i][0], os_list[i][1]))
+        
+    for i in range(len(knn_clf)):
+        pred = classification_report_imbalanced(y_test, knn_clf[i].predict(X_test))
+        knn_df = knn_df.append(report_to_df(pred))
+        #calc auc
+        prob = knn_clf[i].predict_proba(X_test)[:,1]
+        fpr, tpr, thresholds = roc_curve(y_test, prob)
+        roc_auc_area = auc(fpr, tpr)
+        #print('AUC={}'.format(roc_auc_area))
+        auc_list.append(roc_auc_area)
 
-auc_df = pd.DataFrame(auc_list)
+    auc_df = pd.DataFrame(auc_list)
 
-# export resualt
-svm_df.to_csv(svm_out)
-knn_df.to_csv(knn_out)
-auc_df.to_csv(auc_out)
-print("Finished!")
+    # export resualt
+    svm_df.to_csv(svm_out)
+    knn_df.to_csv(knn_out)
+    auc_df.to_csv(auc_out)
