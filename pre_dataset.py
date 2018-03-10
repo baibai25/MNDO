3c3fccbc980687887652a9eb9dac686a54ff24f0
baibai25 <kamuy.camel.0225@gmail.com>
2018-03-10 15:46:01 +0900

    Update
    Added function : preprocessing all files in a directory

--- pre_dataset.py
+++ pre_dataset.py
@@ -1,45 +1,50 @@
 # coding: utf-8
 import sys
-import os.path
+import os
 
-file = open(sys.argv[1], 'r').readlines()
-basename = os.path.basename(sys.argv[1])
-name = os.path.splitext(basename)
-output = 'Predataset/out_{}.csv'.format(name[0])
+directory = sys.argv[1]
+filelist = os.listdir(directory)
 
-file2=[]
-for line in file:
-    #extract attribute
-    if (line.find('@inputs') != -1):
-        attribute = line.replace('@inputs', '')
-        attribute = attribute.replace(' ', '')
-        attribute = attribute.strip()
-        attribute += ',Label\n'
-        #print(attribute)
-    elif (line.find('@input') != -1):
-        attribute = line.replace('@input', '')
-        attribute = attribute.replace(' ', '')
-        attribute = attribute.strip()
-        attribute += ',Label\n'
+for a in filelist:
+    path = os.path.join(directory, a)
 
-    #delete strings
-    if (line.find('@') != -1):
-        del line
-    #replace class label
-    #positive=1, negative=0
-    else:
-        if (line.find('positive') != -1):
-            pos = line.replace('positive', '1')
-            file2.append(pos)
-        elif (line.find('negative') != -1):
-            neg = line.replace('negative', '0')
-            file2.append(neg)
-print(file2)
-file2.insert(0, attribute)
+    file = open(path, 'r').readlines()
+    basename = os.path.basename(path)
+    name = os.path.splitext(basename)
+    output = 'Predataset/{}.csv'.format(name[0])
 
+    file2 = []
+    attribute = ''
+    for line in file:
+        #extract attribute
+        if (line.find('@inputs') != -1):
+            attribute = line.replace('@inputs', '')
+            attribute = attribute.replace(' ', '')
+            attribute = attribute.strip()
+            attribute += ',Label\n'
+            #print(attribute)
+        elif (line.find('@input') != -1):
+            attribute = line.replace('@input', '')
+            attribute = attribute.replace(' ', '')
+            attribute = attribute.strip()
+            attribute += ',Label\n'
 
-out_file = open(output, 'w')
-for i in range(len(file2)):
-    out_file.write(file2[i])
-out_file.close()
+        #delete strings
+        if (line.find('@') != -1):
+            del line
+        #replace class label
+        #positive=1, negative=0
+        else:
+            if (line.find('positive') != -1):
+                pos = line.replace('positive', '1')
+                file2.append(pos)
+            elif (line.find('negative') != -1):
+                neg = line.replace('negative', '0')
+                file2.append(neg)
+    file2.insert(0, attribute)
+
+    out_file = open(output, 'w')
+    for i in range(len(file2)):
+        out_file.write(file2[i])
+    out_file.close()
 
