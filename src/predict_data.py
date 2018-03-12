534641c504f6a74f5649365bd17e472ce2e2f1f3
baibai25 <kamuy.camel.0225@gmail.com>
2018-03-12 22:26:17 +0900

    Update
    change : averge='binary'

--- src/predict_data.py
+++ src/predict_data.py
@@ -4,14 +4,11 @@ from imblearn import metrics
 
 # calc evaluate metrics
 def calc_metrics(y_test, pred, auc, i):
-        sen_macro = metrics.sensitivity_score(y_test, pred, pos_label=1, average='macro')   
-        sen_micro = metrics.sensitivity_score(y_test, pred, pos_label=1, average='micro')    
-        spe_macro = metrics.specificity_score(y_test, pred, pos_label=1, average='macro')   
-        spe_micro = metrics.specificity_score(y_test, pred, pos_label=1, average='micro')
-        geo_macro = metrics.geometric_mean_score(y_test, pred, pos_label=1, average='macro')   
-        geo_mico = metrics.geometric_mean_score(y_test, pred, pos_label=1, average='micro')
+        sen = metrics.sensitivity_score(y_test, pred, pos_label=1)   
+        spe = metrics.specificity_score(y_test, pred, pos_label=1)   
+        geo = metrics.geometric_mean_score(y_test, pred, pos_label=1)   
         index = ['sm', 'b1', 'b2', 'enn', 'tom', 'ada', 'mnd'] 
-        metrics_list = [index[i], sen_macro, sen_micro, spe_macro, spe_micro, geo_macro, geo_mico, auc]
+        metrics_list = [index[i], sen, spe, geo, auc]
         return metrics_list
 
 # convert classification report to dataframe
